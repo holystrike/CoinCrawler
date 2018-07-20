8c5fab362a646405b4f47e4ca37ce8511a1f6779
holystrike <tjdwn@DESKTOP-TTUB2S5>
2018-07-20 18:24:56 +0900

    create project

---
+++ CoinCrawler/src/main/java/com/coinCrawler/sys/config/CoinCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.coinCrawler.sys.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CoinCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinCrawlerApplication.class, args);
+	}
+}
